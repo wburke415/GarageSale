d9ffbefdf2f63157831bc8718b36969f2b556bf6
Whitman Burke <whitmanburke@gmail.com>
2018-08-05 15:50:07 -0700

    Set up entry file and app & root components

---
+++ frontend/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <h1>Welcome to GarageSale</h1>
+  </div>
+);
+
+export default App;
--- frontend/garage_sale.jsx
+++ frontend/garage_sale.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import configureStore from './store/store';
+import Root from './components/app';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore();
-  ReactDOM.render(<h1>Welcome to GarageSale</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
