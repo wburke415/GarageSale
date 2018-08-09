8249d98e72ec46998800907fb4dcf505f4ec8fbe
Whitman Burke <whitmanburke@gmail.com>
2018-08-08 22:30:10 -0700

    Fixed image switching issue

--- frontend/components/product/product_show_images.jsx
+++ frontend/components/product/product_show_images.jsx
@@ -13,17 +13,19 @@ export default class ProductShowImages extends React.Component {
     }
 
     switchMainImage(e) {
-        let newChildren = this.state.childImages;
+        let newChildren = this.state.childImages.slice(0);
         newChildren[parseInt(e.target.id)] = this.state.mainImage;
 
-        this.setState({mainImage: e.target.id});
+        let newMain = this.state.childImages[e.target.id];
+
+        this.setState({mainImage: newMain});
         this.setState({childImages: newChildren});
     }
 
 
     render() {
         const mainImage = <img className="main-image" src={this.state.mainImage.imageUrl} />;
-        const childImages = this.state.childImages.map((childImage, idx) => <li><img key={idx} className="child-image" src={childImage.imageUrl} /></li>);
+        const childImages = this.state.childImages.map((childImage, idx) => <li key={idx}><img onClick={this.switchMainImage} id={idx} className="child-image" src={childImage.imageUrl} /></li>);
 
         return (
             <div className="product-images-wrapper">
