284738bad457fb2094171778d7eefb8e1dd9c3b0
Whitman Burke <whitmanburke@gmail.com>
2018-08-08 17:21:01 -0700

    Start setting up product images

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,5 @@ User.create(firstname: 'Susan', lastname: 'Burke', username: 'SuperSusan', email
 Product.create(seller_id: 1, category_id: 1, payment_policy_id: 1, shipping_policy_id: 1, return_policy_id: 1, location_id: 1, title: "Banjo-Kazooie", subtitle: "Classic N64 game", sku: "22700001", condition: "Very Good", condition_description: "Game is in great condition. Case and manual included.", auction: true, duration: 7, starting_price: 9.99, bin_price: 14.99, quantity: 1)
 ProductImage.create(product_id: 1, image_url: "https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_7/SI_N64_BanjoKazooie_image1600w.jpg")
 ProductImage.create(product_id: 1, image_url: "https://vignette.wikia.nocookie.net/deathbattlefanon/images/0/01/BanjoKazooieTooie7.png/revision/latest?cb=20160508182152")
+ProductImage.create(product_id: 1, image_url: "https://vignette.wikia.nocookie.net/allstar-royale/images/1/1d/Banjo-Kazooie.png/revision/latest?cb=20160101110450")
+ProductImage.create(product_id: 1, image_url: "https://static.giantbomb.com/uploads/square_small/2/25329/1784022-gnawty.png")
--- frontend/components/product/product_show_images.jsx
+++ frontend/components/product/product_show_images.jsx
@@ -2,8 +2,30 @@ import React from 'react';
 
 const ProductShowImages = (props) => {
     const mainImage = <img className="main-image" src={props.productImages[0].imageUrl} />;
+    let childImages = [];
+    
+    for (let i = 1; i < props.productImages.length; i++) {
+        childImages.push(
+        <li>
+            <img className="child-image" src={props.productImages[i].imageUrl} />
+        </li>
+        );
+    }
+    
+    return (
+        <div className="product-images-wrapper">
+            {mainImage}
+            <ul className="child-images-list">
+                {childImages.map(image => image)}
+            </ul>
 
-    return mainImage;
+            <span className="product-sell-button">
+                <i class="fas fa-dollar-sign"></i>
+                <p>Have one to sell?</p>
+                <button>Sell now</button>
+            </span>
+        </div>
+    );
 };
 
 export default ProductShowImages;
