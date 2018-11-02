12c92ffab7b1311628c0baf0ae53bb7ae684f79a
Whitman Burke <whitmanburke@gmail.com>
2018-11-01 21:38:12 -0700

    Update gems

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+
 import {
   Route,
-  Redirect,
   Switch,
-  Link,
-  HashRouter
 } from 'react-router-dom';
 
 import { AuthRoute, ProtectedRoute } from '../utils/route_util';
